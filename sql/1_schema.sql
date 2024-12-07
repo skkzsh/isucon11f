24fa357e662b30ea50617c468b8e1249c21bbddf
skkzsh <skkzsh12@gmail.com>
2024-12-07 21:05:43 +0900

    FKを外してみる #1

--- sql/1_schema.sql
+++ sql/1_schema.sql
@@ -29,17 +29,17 @@ CREATE TABLE `courses`
     `day_of_week` ENUM ('monday', 'tuesday', 'wednesday', 'thursday', 'friday') NOT NULL,
     `teacher_id`  CHAR(26)                                                      NOT NULL,
     `keywords`    TEXT                                                          NOT NULL,
-    `status`      ENUM ('registration', 'in-progress', 'closed')                NOT NULL DEFAULT 'registration',
-    CONSTRAINT FK_courses_teacher_id FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`)
+    `status`      ENUM ('registration', 'in-progress', 'closed')                NOT NULL DEFAULT 'registration'
+    -- CONSTRAINT FK_courses_teacher_id FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `registrations`
 (
     `course_id` CHAR(26),
     `user_id`   CHAR(26),
-    PRIMARY KEY (`course_id`, `user_id`),
-    CONSTRAINT FK_registrations_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
-    CONSTRAINT FK_registrations_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    PRIMARY KEY (`course_id`, `user_id`)
+    -- CONSTRAINT FK_registrations_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
+    -- CONSTRAINT FK_registrations_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `classes`
@@ -50,8 +50,8 @@ CREATE TABLE `classes`
     `title`             VARCHAR(255)     NOT NULL,
     `description`       TEXT             NOT NULL,
     `submission_closed` TINYINT(1)       NOT NULL DEFAULT false,
-    UNIQUE KEY `idx_classes_course_id_part` (`course_id`, `part`),
-    CONSTRAINT FK_classes_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+    UNIQUE KEY `idx_classes_course_id_part` (`course_id`, `part`)
+    -- CONSTRAINT FK_classes_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
 );
 
 CREATE TABLE `submissions`
@@ -60,9 +60,9 @@ CREATE TABLE `submissions`
     `class_id`  CHAR(26)     NOT NULL,
     `file_name` VARCHAR(255) NOT NULL,
     `score`     TINYINT UNSIGNED,
-    PRIMARY KEY (`user_id`, `class_id`),
-    CONSTRAINT FK_submissions_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-    CONSTRAINT FK_submissions_class_id FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`)
+    PRIMARY KEY (`user_id`, `class_id`)
+    -- CONSTRAINT FK_submissions_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    -- CONSTRAINT FK_submissions_class_id FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`)
 );
 
 CREATE TABLE `announcements`
@@ -70,8 +70,8 @@ CREATE TABLE `announcements`
     `id`         CHAR(26) PRIMARY KEY,
     `course_id`  CHAR(26)     NOT NULL,
     `title`      VARCHAR(255) NOT NULL,
-    `message`    TEXT         NOT NULL,
-    CONSTRAINT FK_announcements_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+    `message`    TEXT         NOT NULL
+    -- CONSTRAINT FK_announcements_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
 );
 
 CREATE TABLE `unread_announcements`
@@ -79,7 +79,7 @@ CREATE TABLE `unread_announcements`
     `announcement_id` CHAR(26)   NOT NULL,
     `user_id`         CHAR(26)   NOT NULL,
     `is_deleted`      TINYINT(1) NOT NULL DEFAULT false,
-    PRIMARY KEY (`announcement_id`, `user_id`),
-    CONSTRAINT FK_unread_announcements_announcement_id FOREIGN KEY (`announcement_id`) REFERENCES `announcements` (`id`),
-    CONSTRAINT FK_unread_announcements_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+    PRIMARY KEY (`announcement_id`, `user_id`)
+    -- CONSTRAINT FK_unread_announcements_announcement_id FOREIGN KEY (`announcement_id`) REFERENCES `announcements` (`id`),
+    -- CONSTRAINT FK_unread_announcements_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
