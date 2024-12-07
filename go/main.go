43127c8eb352ed56b18df6184f40084df9ee12d8
skkzsh <skkzsh12@gmail.com>
2024-12-07 22:03:56 +0900

    Revert TODO about slow query

--- go/main.go
+++ go/main.go
@@ -1607,6 +1607,7 @@ func (h *handlers) GetAnnouncementDetail(c echo.Context) error {
 		return c.String(http.StatusNotFound, "No such announcement.")
 	}
 
+	// TODO: slow query (回数が多い)
 	// if _, err := tx.Exec("UPDATE `unread_announcements` SET `is_deleted` = true WHERE `announcement_id` = ? AND `user_id` = ?", announcementID, userID); err != nil {
 	if _, err := h.DB.Exec("UPDATE `unread_announcements` SET `is_deleted` = true WHERE `announcement_id` = ? AND `user_id` = ?", announcementID, userID); err != nil {
 		c.Logger().Error(err)
