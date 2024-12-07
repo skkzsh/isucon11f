66f2507add1a9ec6f6ab8cc20fa04714242cb1c6
skkzsh <skkzsh12@gmail.com>
2024-12-07 22:34:08 +0900

    Solve N+1 (total_score by courses.id) #1

--- go/main.go
+++ go/main.go
@@ -595,6 +595,12 @@ type SubmissionsClassCount struct {
 	Count   int    `db:"count"`
 }
 
+type UserCourseTotal struct {
+	UserID   string `db:"user_id"`
+	CourseID string `db:"course_id"`
+	Total    int    `db:"total_score"`
+}
+
 // GetGrades GET /api/users/me/grades 成績取得
 func (h *handlers) GetGrades(c echo.Context) error {
 	userID, _, _, err := getUserInfo(c)
@@ -614,9 +620,29 @@ func (h *handlers) GetGrades(c echo.Context) error {
 		SubmissionsClassCountMap[s.ClassID] = s.Count
 	}
 
+	var query string
+
+	// 科目ごとの学生のTotalScore一覧を取得
+	var UserCourseTotalList []UserCourseTotal
+	query = "SELECT `users`.`id` AS user_id, `courses`.`id` AS `course_id`, IFNULL(SUM(`submissions`.`score`), 0) AS `total_score`" +
+		" FROM `users`" +
+		" JOIN `registrations` ON `users`.`id` = `registrations`.`user_id`" +
+		" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
+		" LEFT JOIN `classes` ON `courses`.`id` = `classes`.`course_id`" +
+		" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
+		" GROUP BY `users`.`id`, `courses`.`id`"
+	if err := h.DB.Select(&UserCourseTotalList, query); err != nil {
+		c.Logger().Error(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	UserCourseTotalMap := make(map[string][]int, len(UserCourseTotalList))
+	for _, t := range UserCourseTotalList {
+		UserCourseTotalMap[t.CourseID] = append(UserCourseTotalMap[t.CourseID], t.Total)
+	}
+
 	// 履修している科目一覧取得
 	var registeredCourses []Course
-	query := "SELECT `courses`.*" +
+	query = "SELECT `courses`.*" +
 		" FROM `registrations`" +
 		" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
 		" WHERE `user_id` = ?"
@@ -677,21 +703,21 @@ func (h *handlers) GetGrades(c echo.Context) error {
 			}
 		}
 
-		// TODO: slow query
 		// この科目を履修している学生のTotalScore一覧を取得
 		var totals []int
-		query := "SELECT IFNULL(SUM(`submissions`.`score`), 0) AS `total_score`" +
-			" FROM `users`" +
-			" JOIN `registrations` ON `users`.`id` = `registrations`.`user_id`" +
-			" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
-			" LEFT JOIN `classes` ON `courses`.`id` = `classes`.`course_id`" +
-			" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
-			" WHERE `courses`.`id` = ?" +
-			" GROUP BY `users`.`id`"
-		if err := h.DB.Select(&totals, query, course.ID); err != nil {
-			c.Logger().Error(err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
+		// query := "SELECT IFNULL(SUM(`submissions`.`score`), 0) AS `total_score`" +
+		// 	" FROM `users`" +
+		// 	" JOIN `registrations` ON `users`.`id` = `registrations`.`user_id`" +
+		// 	" JOIN `courses` ON `registrations`.`course_id` = `courses`.`id`" +
+		// 	" LEFT JOIN `classes` ON `courses`.`id` = `classes`.`course_id`" +
+		// 	" LEFT JOIN `submissions` ON `users`.`id` = `submissions`.`user_id` AND `submissions`.`class_id` = `classes`.`id`" +
+		// 	" WHERE `courses`.`id` = ?" +
+		// 	" GROUP BY `users`.`id`"
+		// if err := h.DB.Select(&totals, query, course.ID); err != nil {
+		// 	c.Logger().Error(err)
+		// 	return c.NoContent(http.StatusInternalServerError)
+		// }
+		totals = UserCourseTotalMap[course.ID]
 
 		courseResults = append(courseResults, CourseResult{
 			Name:             course.Name,
